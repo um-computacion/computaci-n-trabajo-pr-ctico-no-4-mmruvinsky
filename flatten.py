7191ffb108593b2f44fe2594e0bfa39d04df4a41
Miguel <mmruvinsky@gmail.com>
2025-05-01 18:21:21 -0300

    Se implementa la función flatten para aplanar listas de forma recursiva

--- flatten.py
+++ flatten.py
@@ -3,11 +3,18 @@ def flatten(lista):
     for i in lista:
         if isinstance(i, list):
             resultado.extend(flatten(i))
+        elif isinstance(i, tuple):
+            resultado.extend(flatten(list(i)))
+        elif isinstance(i, dict):
+            for key, value in i.items():
+                resultado.append(key)
+                resultado.extend(flatten([value]))
+
         else:
             resultado.append(i)
     return resultado
 
 
-print (flatten([1, 2, [3, 4], [5, [6, 7]]]))
+print (flatten([1, (2, 3), {'a': 4, 'b': 5}, [6, [7, 8]]]))
 
 """funcion recursiva para aplanar una lista de listas"""
--- test_flatten.py
+++ test_flatten.py
@@ -1,2 +1,17 @@
 import unittest
 from flatten import flatten
+
+class TestFlatten(unittest.TestCase):
+    def test_flatten(self):
+        self.assertEqual(flatten([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+        self.assertEqual(flatten([1, (2, 3), {'a': 4, 'b': 5}, [6, [7, 8]]]), [1, 2, 3, 'a', 4, 'b', 5, 6, 7, 8])
+        self.assertEqual(flatten([1, 2, [3, 4], [5, [6, 7]]]), [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(flatten([]), [])
+        self.assertEqual(flatten([1, [2], [[3]], [[[4]]]]), [1, 2, 3, 4])
+        self.assertEqual(flatten([[[[1]]], [[[[2]]]], [[[[[3]]]]]]), [1, 2, 3])
+        self.assertEqual(flatten([1, [2], [3], [4], [5]]), [1, 2, 3, 4, 5])
+        self.assertEqual(flatten([1, [2, [3, [4]]], 5]), [1, 2, 3, 4, 5])
+
+
+if __name__ == '__main__':
+    unittest.main() 
